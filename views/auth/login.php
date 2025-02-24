0a3f54a6a22459ba4030beded4bafaa73c94a4a5
Ivan Guerrero <ivangvprogrammer@gmail.com>
2025-02-23 23:32:43 -0500

    feat(auth): Crea vista de Login con formulario y enlaces de registro

--- views/auth/login.php
+++ views/auth/login.php
@@ -2,8 +2,24 @@
   <h2 class="auth__heading">
     <?php echo $titulo; ?>
   </h2>
-  <p class="" auth__texto>Incia sesion en DevWebcamp</p>
+  <p class="auth__texto">Inicia Sesión en DevWebcamp</p>
 
-  <form class="formulario"></form>
+  <form class="formulario">
+    <div class="formulario__campo">
+      <label for="email" class="formulario__label">Email</label>
+      <input type="email" class="formulario__input" placeholder="Tu Email" id="email" name="email" />
+    </div>
 
+    <div class="formulario__campo">
+      <label for="password" class="formulario__label">Password</label>
+      <input type="password" class="formulario__input" placeholder="Tu Password" id="password" name="password" />
+    </div>
+
+    <input type="submit" class="formulario__submit" value="Iniciar Sesión">
+  </form>
+
+  <div class="acciones">
+    <a href="/registro" class="acciones__enlace">¿Aún no tenés cuenta? Obtener una</a>
+    <a href="/olvide" class="acciones__enlace">¿Olvisate tu Password?</a>
+  </div>
 </main>
