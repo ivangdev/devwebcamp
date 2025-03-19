07bbc18dc6f39cf027e650f1f4bd29779eaf074d
Ivan Guerrero <ivangvprogrammer@gmail.com>
2025-03-18 22:36:05 -0500

    feat(header): Añadiendo el HTML del Header

---
+++ views/templates/header.php
@@ -0,0 +1,25 @@
+<header class="header">
+  <div class="header__contenedor">
+    <!-- Enlaces de navegación para autenticación -->
+    <nav class="header__navegacion">
+      <a href="/registro" class="header__enlace">Registro</a>
+      <a href="/login" class="header__enlace">Iniciar Sesión</a>
+    </nav>
+
+    <!-- Logo del sitio y enlace a inicio -->
+    <div class="header__contenido">
+      <a href="/">
+        <h1 class="header__logo"> <!-- Logo usando entidad HTML -->
+          &#60;DevWebCamp />
+        </h1>
+      </a>
+
+      <!-- Información del evento -->
+      <p class="header__texto">Octubre 5-6 - 2025</p>
+      <p class="header__texto header__texto--modalidad">En Línea - Presencial</p>
+
+      <!-- Botón de llamada a la acción -->
+      <a href="/registro" class="header__boton">Comprar Pase</a>
+    </div>
+  </div>
+</header>
