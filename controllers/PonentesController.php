d794b500c8984ace4dc4704afbe9eae969c76b5f
Ivan Guerrero <ivangvprogrammer@gmail.com>
2025-05-23 22:27:40 -0500

    feat(admin): agregar funcionalidad para eliminar ponentes desde el panel de administración
    - Implementa ruta POST y método en PonentesController para gestionar la eliminación de ponentes.
    - Actualiza la vista de administración para incluir un formulario de eliminación con campo oculto de ID.
    - Permite a los administradores eliminar ponentes del sistema de forma segura y sencilla desde la interfaz.

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -96,11 +96,70 @@ class PonentesController
 
     // debuguear($redes);
 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      // Leer imagen
+      if (!empty($_FILES['imagen']['tmp_name'])) {
+        $carpeta_imagenes = '../public/img/speakers';
+
+        // Crear la carpeta si no existe
+        if (!is_dir($carpeta_imagenes)) {
+          mkdir($carpeta_imagenes, 0777, true);
+        }
+
+        $manager = new Image(Driver::class);
+        $imagen_png = $manager->read($_FILES['imagen']['tmp_name'])->contain(800, 800)->encodeByExtension('png', 80);
+        $imagen_webp = $manager->read($_FILES['imagen']['tmp_name'])->contain(800, 800)->encodeByExtension('webp', 80);
+
+        $nombre_imagen = md5(uniqid(rand(), true));
+        $_POST['imagen'] = $nombre_imagen;
+      } else {
+        $_POST['imagen'] = $ponente->imagen_actual; // Si no se sube una nueva imagen, se mantiene la actual
+      }
+
+      $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
+      $ponente->sincronizar($_POST);
+      $alertas = $ponente->validar();
+
+      // Validar que la imagen no sea la misma que la actual
+      if (empty($alertas)) {
+        // Si la imagen es la misma que la actual, no se guarda una nueva
+        if (isset($nombre_imagen)) {
+          // Guardar las imagenes
+          $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . '.png');
+          $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . '.webp');
+        }
+        // Guardar en la base de datos
+        $resultado = $ponente->guardar();
+
+        if ($resultado) {
+          header('Location: /admin/ponentes');
+        }
+      }
+    }
+
     $router->render('admin/ponentes/editar', [
       'titulo' => 'Actualizar Ponente',
       'alertas' => $alertas,
       'ponente' => $ponente,
       'redes' => json_decode($ponente->redes)
     ]);
+  }
+
+  public static function eliminar()
+  {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $id = $_POST['id'];
+      $ponente = Ponente::find($id);
+
+      if (!isset($ponente)) {
+        header('Location: /admin/ponentes');
+      }
+
+      // debuguear($ponente);
+      $resultado = $ponente->eliminar();
+      if ($resultado) {
+        header('Location: /admin/ponentes');
+      }
+    }
   }
 }
