dc6028bc4ce13d28765e3e79d966590aeca12f1e
Ivan Guerrero <ivangvprogrammer@gmail.com>
2025-05-23 19:00:14 -0500

    feat(admin): mejorar coherencia y estilos de tablas en la vista de ponentes
    - Crea _table.scss con estilos de tabla reutilizables para la administración
    - Actualiza _index.scss para importar los nuevos estilos de tabla
    - Refactoriza admin/ponentes/index.php para usar clases BEM en tablas
    - Mejora la coherencia visual y la estructura de las tablas en el panel de administración

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -12,7 +12,7 @@ class PonentesController
   public static function index(Router $router)
   {
     $ponentes = Ponente::all();
-    
+
 
     $router->render('admin/ponentes/index', [
       'titulo' => 'Ponentes / Conferencistas',
