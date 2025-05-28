973b92b98c9db93cbb7dfc0a3e685ea47d54e2e5
Ivan Guerrero <ivangvprogrammer@gmail.com>
2025-05-27 23:50:50 -0500

    feat(admin): agregar vista y ruta para la creación de eventos en el panel de administración
    - Implementa la funcionalidad de alta de eventos con nueva ruta, método en el controlador y vistas dedicadas.
    - Actualiza el índice de eventos para incluir el botón «Agregar Evento», facilitando la gestión y el flujo de trabajo del administrador.

---
+++ views/admin/eventos/formulario.php
@@ -0,0 +1,24 @@
+<fieldset class="formulario__fieldset">
+  <legend class="formulario__legend">Información del Evento</legend>
+
+  <div class="formulario__campo">
+    <label for="nombre" class="formulario__label">Nombre Evento</label>
+    <input
+      type="text"
+      class="formulario__input"
+      id="nombre"
+      name="nombre"
+      placeholder="Nombre Evento" />
+  </div>
+
+  <div class="formulario__campo">
+    <label for="descripcion" class="formulario__label">Descripción</label>
+    <textarea
+      class="formulario__input"
+      id="descripcion"
+      name="descripcion"
+      placeholder="Descripción Evento"
+      rows="8"></textarea>
+
+  </div>
+</fieldset>
